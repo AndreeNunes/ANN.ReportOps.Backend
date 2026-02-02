93f38392e1d005536b4760bc53a442d5413214e8
Andre Nicola Nunes <andre.nicola.nunes@gmail.com>
2026-02-02 19:07:54 -0300

    Add CORS support in main.py to enable cross-origin requests for API endpoints.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_cors import CORS
+
 from src.controller.ordem_service_attachment_controller import ordem_service_attachment_controller
 from src.controller.equipament_controller import equipament_controller
 from src.controller.authenticate_controller import authenticate_controller
@@ -7,6 +9,10 @@ from src.controller.report_controller import report_controller
 from src.controller.client_controller import client_controller
 
 app = Flask(__name__)
+
+CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 app.register_blueprint(authenticate_controller, url_prefix="/v1")
 app.register_blueprint(company_controller, url_prefix="/v1/company")
 app.register_blueprint(report_controller, url_prefix="/v1/report")
